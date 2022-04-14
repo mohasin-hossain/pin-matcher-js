20924b9ccf623f4fa9a4a05654d1641c692b6622
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-04-14 12:40:16 +0600

    Backspace feature added

--- js/script.js
+++ js/script.js
@@ -19,6 +19,9 @@ document.getElementById("key-pad").addEventListener("click", function (event) {
   if (isNaN(number)) {
     if (number == "C") {
       calcInput.value = "";
+    } else if (number == "<") {
+      const updatedNumber = Math.floor(calcInput.value / 10);
+      calcInput.value = updatedNumber;
     }
   } else {
     const previousNumber = calcInput.value;
